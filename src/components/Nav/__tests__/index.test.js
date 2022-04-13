681f0cb842885285df2fdd6f9836bf998c2945f0
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-04-13 01:19:18 -0500

    added code to display the photos and also reinstalled the files to work with the older environment

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,35 +7,31 @@ import Nav from '..';
 afterEach(cleanup);
 
 describe('Nav component', () => {
-    // baseline test
-    it('renders', () => {
-        render(<Nav />);
-    });
+  it('renders', () => {
+    render(<Nav />);
+  });
 
-    // snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
-        // assert value comparison
-        expect(asFragment()).toMatchSnapshot();
-    });
-});
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
 
 describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-        // Arrange
-        const { getByLabelText } = render(<Nav />);
-        // Assert
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-});
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})
 
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
-        // arrange
-        const { getByTestId } = render(<Nav />);
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
 
-        // assert
-        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        expect(getByTestId('about')).toHaveTextContent('About me');
-    });
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
 })
