e6752d7d3eeabfa26db43b3807afe313dcc4ea6b
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-04-13 01:39:50 -0500

    added/updated tests

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Gallery from '..'
+
 const portrait = { name: "portraits", description: "Portraits of people in my life" };
 
 afterEach(cleanup)
@@ -17,9 +18,10 @@ describe('Gallery component', () => {
     const { asFragment } = render(<Gallery currentCategory={portrait} />)
     expect(asFragment()).toMatchSnapshot()
   })
+
+  it('renders', () => {
+    const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+    expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+  })
 });
 
-it('renders', () => {
-  const { getByTestId } = render(<Gallery currentCategory={portrait} />)
-  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
-})
